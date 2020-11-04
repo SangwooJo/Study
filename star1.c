71dfa56c18a0754c2a7ee2b0b86faac23550e4fc
SWJ <50561255+SangwooJo@users.noreply.github.com>
2020-11-04 13:52:37 +0900

    New

---
+++ star1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void star(int);
+
+void main()
+{
+	star(5);
+}
+
+void star(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
