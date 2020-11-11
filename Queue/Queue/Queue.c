d0a1373afc0f54580c9bc16aa8d64fd881693861
SWJ <50561255+SangwooJo@users.noreply.github.com>
2020-11-12 01:09:45 +0900

    Update Queue.c

--- Queue/Queue/Queue.c
+++ Queue/Queue/Queue.c
@@ -1,27 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MaxSizeNumber 5
+#include <stdbool.h>
+#define MaxSizeNumber 100
 
 int arrNumber[MaxSizeNumber];
 int glovalKey = 0;
+int queueCount = 0;
 
-int input(int number)
+int push(int number)
 {
     arrNumber[glovalKey] = number;
     glovalKey++;
+}
+void pop()
+{
+    if (glovalKey == queueCount)
+    {
+        printf("empty\n");
+    }
+    else
+    {
+        printf("%i\n", arrNumber[queueCount]);
+        queueCount++;
+    }
 }
 
 int main(void)
 {
     int c = MaxSizeNumber - 1;
-    input(2);
-    input(4);
-    input(6);
-    input(8);
-    input(10);
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    push(6);
+    push(7);
+    push(8);
+    push(9);
 
-    for (int i = 0; i <= c; i++)
-    {
-        printf("%i\n", arrNumber[i]);
-    }
+    pop();
+    pop();
+    push(8);
+    pop();
+    pop();
+    pop();
+    pop();
+    pop();
+
+    push(22);
+    pop();
+
+    pop();
+    pop();
+    pop();
+    pop();
+    pop();
+
+    push(123);
+    push(456);
+    push(789);
+    pop();
+
+    push(20);
+    pop();
+    pop();
+    pop();
 }
