af1ca56a67b6ab3d937c623fb0878ba9e02ade48
SWJ <50561255+SangwooJo@users.noreply.github.com>
2020-11-11 16:33:34 +0900

    u

--- Sort/Test/Sort.c
+++ Sort/Test/Sort.c
@@ -34,24 +34,20 @@ int main(void)
 void bubbleSort(int list[], int n)
 {
     int temp;
-    bool flag = true;
+    bool nB = false;
 
-    for(int i = 0;i < n  ; i++)
-    {
-        for (int j = 0; j < n-1; j++)
+    for (int i = 0; i < n; i++)
+    {        
+        for (int j = 0; j < n - 1; j++)
         {
             if (list[j] > list[j + 1])
             {
                 temp = list[j];
                 list[j] = list[j + 1];
                 list[j + 1] = temp;
-                flag = false;
-            }                        
-        }
-        if (!flag)
-        {
-            break;
-        }
+                nB = true;
+            }
+        }        
     }
 }
 
