efc15b6af3f99f77b118fc6ad32b59e282798148
SWJ <50561255+SangwooJo@users.noreply.github.com>
2020-11-13 01:10:05 +0900

    u

--- LinkedList/LinkedList/linkedlistSource.c
+++ LinkedList/LinkedList/linkedlistSource.c
@@ -7,71 +7,74 @@ typedef struct node // 연결 리스트의 노드 구조체
     struct  node* next; // 다음 노드의 주소를 저장할 포인터
 }
 node;
-
-void addLinkedList(node* target, int data);
-void updateLinkedList(node* target, int linkedCount, int changeValue);
-void deleteLinkedList(node* target, int linkedCount);
-void selectLinkedList(node* target, int linkedCount);
+struct  node* Head;
+void addLinkedHead(node* target, int data);
+void updateLinkedHead(node* target, int linkedCount, int changeValue);
+void deleteLinkedHead(int linkedCount);
+int selectLinkedHead(node* target, int linkedCount);
 
 int main(void)
 {
     int count = 1;
-    node* list = malloc(sizeof(node)); //target 노드가 될 노드에 메모리 할당 
-    if (list == NULL)
+    int nCount;
+    Head = malloc(sizeof(node)); //target 노드가 될 노드에 메모리 할당 
+    if (Head == NULL)
     {
         return 1;
     }
-    list->next = NULL;  // 처음시작시 target 노드의 다음노드에 초기화 
+    Head->next = NULL;  // 처음시작시 target 노드의 다음노드에 초기화 
 
-    addLinkedList(list, 1);
-    addLinkedList(list, 2);
-    addLinkedList(list, 3);
-    addLinkedList(list, 4);
-    addLinkedList(list, 5);
-    addLinkedList(list, 6);
-    addLinkedList(list, 7);
-    addLinkedList(list, 8);
-    addLinkedList(list, 9);
-    addLinkedList(list, 10);
-    addLinkedList(list, 11);
-    addLinkedList(list, 12);
-    addLinkedList(list, 13);
-    addLinkedList(list, 14);
-    addLinkedList(list, 15);
-    addLinkedList(list, 99);
-    addLinkedList(list, 98);
-    addLinkedList(list, 97);
-    addLinkedList(list, 96);
-    addLinkedList(list, 95);
+    addLinkedHead(Head, 1);
+    addLinkedHead(Head, 2);
+    addLinkedHead(Head, 3);
+    addLinkedHead(Head, 4);
+    addLinkedHead(Head, 5);
+    addLinkedHead(Head, 6);
+    addLinkedHead(Head, 7);
+    addLinkedHead(Head, 8);
+    addLinkedHead(Head, 9);
+    addLinkedHead(Head, 10);
+    addLinkedHead(Head, 11);
+    addLinkedHead(Head, 12);
+    addLinkedHead(Head, 13);
+    addLinkedHead(Head, 14);
+    addLinkedHead(Head, 15);
+    addLinkedHead(Head, 16);
+    addLinkedHead(Head, 17);
+    addLinkedHead(Head, 18);
+    addLinkedHead(Head, 19);
+    addLinkedHead(Head, 20);
 
-    selectLinkedList(list, 17);
-    updateLinkedList(list, 17, 22);
-    selectLinkedList(list, 17);
+    updateLinkedHead(Head, 1, 22);
 
-    selectLinkedList(list, 5);
-    deleteLinkedList(&list,list, 5);
-    selectLinkedList(list, 5);
+    deleteLinkedHead(5);
 
-    //selectLinkedList(list, 5);
-    //selectLinkedList(list, 17);
 
-    node* tmp = list->next; // target 노드를 복사할 임시 노드 생성
-                                        // 복사하는 이유는 노드의 마지막 다음 노드의 NULL값 방지
+    node* tmp = Head->next; // target 노드를 복사할 임시 노드 생성
+                                             // 복사하는 이유는 노드의 마지막 다음 노드의 NULL값 방지
 
-    while (tmp->next != NULL) // 마지막 다음노드에서 부터 디스플레이 loop
+    while (1) // 마지막 다음노드에서 부터 디스플레이 loop
     {
-        printf("%i번째 노드의 값 %i\n", count, tmp->number);
-        tmp = tmp->next;
-        count++;
+        if (tmp->next != NULL)
+        {
+            printf("노드의 값: %i\n", selectLinkedHead(Head, count));
+            tmp = tmp->next;
+            count++;
+        }
+        else
+        {
+            printf("노드의 값: %i\n", selectLinkedHead(Head, count));
+            break;
+        }
     }
 
-    //free(tmp);
-    free(list);
+    free(tmp);
+    free(Head);
 
     return 0;
 }
 
-void addLinkedList(node* target, int data)  // 'C'
+void addLinkedHead(node* target, int data)  // 'C'
 {
     node* newNode = malloc(sizeof(node)); // 새노드에 메모리 할당
     if (newNode == NULL)
@@ -79,57 +82,54 @@ void addLinkedList(node* target, int data)  // 'C'
         return 1;
     }
     newNode->next = target->next; // 새 노드의 다음 노드에 target 노드의 다음 노드를 지정
+    newNode->next = Head->next;
     newNode->number = data; // 새노드에 매개변수 n의 값 저장 
 
     target->next = newNode; // target 노드의 다음 노드에 새 노드를 할당
 }
 
-void updateLinkedList(node* target, int linkedCount, int changeValue) // 'U'
+void updateLinkedHead(node* target, int linkedCount, int changeValue) // 'U'
 {
     node* selectNode = target->next;
     if (selectNode == NULL)
     {
         return 1;
     }
-
-    for (int i = 1; i <= linkedCount - 1; i++)
+    for (int i = 1; i <= linkedCount; i++)
     {
         selectNode = selectNode->next;
     }
-    printf("선택된 노드 %i번째 %i값", linkedCount, selectNode->number);
     selectNode->number = changeValue;
-    printf(" -> %i값으로 변경 되었습니다.\n", changeValue);
 }
 
-void selectLinkedList(node* target, int linkedCount) // 'R'
+int selectLinkedHead(node* target, int linkedCount) // 'R'
 {
     node* selectNode = target;
     if (selectNode == NULL)
     {
         return 1;
+    }
+    for (int i = 1; i <= linkedCount; i++)
+    {
+        selectNode = selectNode->next;
+    }
+    return selectNode->number;
+}
+
+void deleteLinkedHead(int linkedCount) // 'D'
+{
+    node* temp1 = Head;
+    if (linkedCount == 1)
+    {
+        Head = temp1->next;
+        free(temp1);
+        return;
     }
     for (int i = 1; i <= linkedCount - 1; i++)
     {
-        selectNode = selectNode->next;
+        temp1 = temp1->next;
     }
-
-    printf("%i번째 노드가 선택되었습니다. 값은 %i 입니다.\n", linkedCount, selectNode->number);
+    node *temp2 = temp1->next;
+    temp1->next = temp2->next;
+    free(temp2);
 }
-
-void deleteLinkedList(node* target, int linkedCount)
-{
-    node* selectNode = target;
-    if (selectNode == NULL)
-    {
-        return 1;
-    }
-    for (int i = 0; i < linkedCount - 1; i++)
-    {
-        selectNode = selectNode->next;
-    }
-    node* delete = selectNode->next;
-    selectNode->next = selectNode->next->next;
-    printf("%i", selectNode->next);
-    free(delete);    
-}
-
