452d43b0b749d82d8dc342c7ec15beea2d0dfac3
SWJ <50561255+SangwooJo@users.noreply.github.com>
2020-11-16 14:16:02 +0900

    U

--- C Language/Strmp/strcmp/strcmp/strcmp.c
+++ C Language/Strmp/strcmp/strcmp/strcmp.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
-
+int strcmp(char* s1, char* s2);
 int main(void)
 {
-    char* s1 = "sangwoo";
-    char* s2 = "sangwoo";
+    char* s1 = "saa1ngwoo";
+    char* s2 = "sa1ngwoo";
 
-    if (strmp(s1, s2) == 0)
+    if (strcmp(s1, s2) == 0)
     {
         printf("[%s] == [%s]\n", s1, s2);
         printf("Success in strmp function");
     }
 }
-
-int strmp(char* s1, char* s2)
+int strcmp(char* s1, char* s2)
 {
     int countNumber = 1;
     char c1, c2;
 
+    char* xAdress = &s1;
+    char* yAdress = &s2;
+
     while (1)
-    {        
+    {
         c1 = *s1++;
-        c2 = *s2++;        
+        c2 = *s2++;
         if (c1 != c2)
         {
             printf("it's not same\n");
             printf("you have to fix---> %i : %c != %c\n", countNumber, c1, c2);
             return 1;
         }
-        else
-        {            
+        if (*xAdress == *yAdress)
+        {
             printf("it's same\n");
             return 0;
         }
