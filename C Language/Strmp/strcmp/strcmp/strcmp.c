d699d4e44cb69dfde9df8c411603ad0e06832cf2
SWJ <50561255+SangwooJo@users.noreply.github.com>
2020-11-17 23:39:04 +0900

    U

--- C Language/Strmp/strcmp/strcmp/strcmp.c
+++ C Language/Strmp/strcmp/strcmp/strcmp.c
@@ -3,14 +3,14 @@
 int strcmp(char* s1, char* s2);
 int main(void)
 {
-    char* s1 = "san4gwoo";
-    char* s2 = "saa2ngwoo";
+    char* s1 = "sangwoo";
+    char* s2 = "sangwoo2";
 
     if (strcmp(s1, s2) == 0)
     {
         printf("same....[%s] == [%s]\n", s1, s2);        
     }
-    if (strcmp(s1, s2) == 1)
+    else
     {
         printf("not same....[%s] != [%s]\n", s1, s2);        
     }
@@ -20,17 +20,18 @@ int strcmp(char* s1, char* s2)
     int countNumber = 1;
     char c1, c2;
 
-    char* xAdress = &s1;
-    char* yAdress = &s2;
-
     while (1)
     {
+        // 문자열의 글자를 하나씩 ++;
         c1 = *s1++;
+        printf("%p -- c1\n", *&s1);
         c2 = *s2++;
+        printf("%p -- c2\n", *&s2);
         if (c1 != c2)
         {
             return 1;
         }
+        // 문자열의 끝까지 도달하면 break
         if (c1 == NULL || c2 == NULL)
         {
             break;
