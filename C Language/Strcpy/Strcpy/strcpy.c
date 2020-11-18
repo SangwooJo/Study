41acde6e2ffdfe25917a40ce7adc3a27650afc56
SWJ <50561255+SangwooJo@users.noreply.github.com>
2020-11-18 09:58:55 +0900

    U

--- C Language/Strcpy/Strcpy/strcpy.c
+++ C Language/Strcpy/Strcpy/strcpy.c
@@ -4,12 +4,14 @@
 
 int main(void)
 {
-    char* s1 = "sangwoo";
+     char* s1 = "sangwoo";
     // 문자열의 마지막 Null값까지 포함해서 메모리 할당
-    char* s2 = malloc(strlen(s1) + 1);
-    
+     char* s2 = malloc(strlen(s1) + 1);
+     int size = 0;
+     size = strlen(s1) + 1;
+
     // s1의 Null값 까지 포함한 문자열 길이(번) 반복문 수행
-    for (int i = 0; i < strlen(s1)+1; i++)
+    for (int i = 0; i < size; i++)
     {
         //문자열의 처음서부터 마지막까지 loop
         s2[i] = s1[i];
